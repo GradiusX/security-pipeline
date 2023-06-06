5a35abd8a2fd4b4021383f054ae17e33277943b4
GradiusX <francescomifsud@gmail.com>
2023-06-06 11:39:33 +0200

    Updated

--- .github/actions/yarn/index.js
+++ .github/actions/yarn/index.js
@@ -1,9 +1,9 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const exec = require('@actions/exec');
+import { getInput, setFailed } from '@actions/core';
+import github from '@actions/github';
+import { exec as _exec } from '@actions/exec';
 
 // Report only this level and above: info|low|moderate|high|critical
-const severityLevel = core.getInput('severity-level');
+const severityLevel = getInput('severity-level');
 
 (async () => {
     let commandOutput = '';
@@ -23,10 +23,10 @@ const severityLevel = core.getInput('severity-level');
         //cwd : './test'
     }
     try{
-        await exec.exec('yarn', ['audit', '--level', severityLevel], options);
+        await _exec('yarn', ['audit', '--level', severityLevel], options);
         console.log("No Security Issues found")
     }catch(error){
         console.log(commandOutput)
-        core.setFailed(error.message)
+        setFailed(error.message)
     }
 })();
