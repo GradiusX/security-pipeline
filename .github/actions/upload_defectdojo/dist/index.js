f422911ab839b51f78602f38f0aa8c4bc04e97cf
GradiusX <francescomifsud@gmail.com>
2023-07-02 01:38:18 +0200

    sdfsdf

--- .github/actions/upload_defectdojo/dist/index.js
+++ .github/actions/upload_defectdojo/dist/index.js
@@ -42634,7 +42634,8 @@ let today = new Date(Date.now());
         formData : {
             "product_name": productName,
             "engagement_name": engagementName,
-            "engagement_end_date":  today.getFullYear()+"-"+today.getMonth()+"-"+(today.getDate()),
+            // "engagement_end_date":  today.getFullYear()+"-"+today.getMonth()+"-"+(today.getDate()),
+            "engagement_end_date":  '2023-6-3',
             "scan_type": scanType,
             "auto_create_context": "true",
             "close_old_findings": "true",
